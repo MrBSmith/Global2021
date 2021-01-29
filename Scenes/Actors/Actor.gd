fd5c2ea0f2b41c88ff28fc7b2ab8b52e5f222320
Hugo Baba <Hugobarette@gmail.com>
2021-01-29 21:29:33 +0100

    Basic player movement

---
+++ Scenes/Actors/Actor.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+class_name Actor
+
+#### ACCESSORS ####
+
+func is_class(value: String): return value == "Actor" or .is_class(value)
+func get_class() -> String: return "Actor"
+
+
+#### BUILT-IN ####
+
+
+
+#### VIRTUALS ####
+
+
+
+#### LOGIC ####
+
+
+
+#### INPUTS ####
+
+
+
+#### SIGNAL RESPONSES ####
---
+++ Scenes/Actors/Player/Player.gd
@@ -0,0 +1,39 @@
+extends Actor
+class_name Player
+
+const SPEED : float = 200.0
+
+var v_movement : float = 0.0
+var h_movement : float = 0.0
+
+#### ACCESSORS ####
+
+func is_class(value: String): return value == "Player" or .is_class(value)
+func get_class() -> String: return "Player"
+
+
+#### BUILT-IN ####
+
+func _physics_process(_delta: float) -> void:
+	var dir = Vector2(v_movement, h_movement).normalized()
+	var velocity = dir * SPEED
+	
+	var __ = move_and_slide(velocity)
+ 
+
+#### VIRTUALS ####
+
+
+
+#### LOGIC ####
+
+
+
+#### INPUTS ####
+
+func _input(_event: InputEvent) -> void:
+	v_movement = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	h_movement = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+
+#### SIGNAL RESPONSES ####
